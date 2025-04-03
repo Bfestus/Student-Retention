c681f668a3cdd6e80446ceba3074f561874ae7b3
Bfestus <f.bigiriman@alustudent.com>
2025-04-03 11:04:12 +0200

    changes

--- src/api/start.sh
+++ src/api/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-uvicorn main:app --host 0.0.0.0 --port $PORT
+pip install --no-cache-dir -r src/api/requirements.txt  # Ensure dependencies are installed
+python -m uvicorn src.api.main:app --host 0.0.0.0 --port $PORT  # Use full module path
